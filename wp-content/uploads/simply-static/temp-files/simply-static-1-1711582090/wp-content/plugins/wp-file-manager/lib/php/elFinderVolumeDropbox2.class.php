f20f5e52da9db57a704e0df6145d27c62db2fd38
rexus.sinonjs <sardar.itagency@gmail.com>
2024-03-28 05:33:53 +0600

    222
    222

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1711582090/wp-content/plugins/wp-file-manager/lib/php/elFinderVolumeDropbox2.class.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;elFinderVolumeDriver&quot; not found in C:\xampp\htdocs\dsr\wp-content\plugins\wp-file-manager\lib\php\elFinderVolumeDropbox2.class.php:16
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\dsr\wp-content\plugins\wp-file-manager\lib\php\elFinderVolumeDropbox2.class.php</b> on line <b>16</b><br>
---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1711582090/wp-content/plugins/wp-file-manager/lib/php/plugins/AutoResize/plugin.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;elFinderPlugin&quot; not found in C:\xampp\htdocs\dsr\wp-content\plugins\wp-file-manager\lib\php\plugins\AutoResize\plugin.php:62
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\dsr\wp-content\plugins\wp-file-manager\lib\php\plugins\AutoResize\plugin.php</b> on line <b>62</b><br>
